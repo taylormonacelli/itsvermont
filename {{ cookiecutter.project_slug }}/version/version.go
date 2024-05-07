8b737bad3b0c0355edc9f42218117fc0d438dd33
Taylor Monacelli <taylormonacelli@gmail.com>
2024-05-07 16:00:12 -0700

    distribute version to all

--- {{ cookiecutter.project_slug }}/version/version.go
+++ {{ cookiecutter.project_slug }}/version/version.go
@@ -1,5 +1,21 @@
 package version
 
+import "fmt"
+
+type BuildInfo struct {
+	Date        string
+	FullGitSHA  string
+	GoVersion   string
+	ShortGitSHA string
+	Version     string
+}
+
+func (bi BuildInfo) String() string {
+	return fmt.Sprintf(`Version: %s, %s
+Build Date: %s
+Go Version: %s`, bi.Version, bi.FullGitSHA, bi.Date, bi.GoVersion)
+}
+
 var (
 	Date        string
 	FullGitSHA  string
@@ -7,3 +23,13 @@ var (
 	ShortGitSHA string
 	Version     string
 )
+
+func GetBuildInfo() BuildInfo {
+	return BuildInfo{
+		Date:        Date,
+		FullGitSHA:  FullGitSHA,
+		GoVersion:   GoVersion,
+		ShortGitSHA: ShortGitSHA,
+		Version:     Version,
+	}
+}
